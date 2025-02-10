e2ce73f4722cb900d553995fb2b5760cd0caad28
Carlson29 <kometacarlson@gmail.com>
2025-02-10 20:25:07 +0000

    creating controller with signUp action

---
+++ Controller/index.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+//use UserDao;
+//require '..\Daos\Dao.php';
+//require '..\business\User.php';
+require '..\Daos\UserDao.php';
+
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+} else {
+    $user = "";
+}
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+}
+if ($action == NULL && $user != NULL) {
+    $action = 'show_signup';
+} elseif ($action == NULL && $user == NULL) {
+    $action = 'show_signup';
+}
+switch ($action) {
+    case "show_signup":
+        include "../view/signUp.php";
+        break;
+    case "do_signup":
+        $userDao = new UserDao("fastjobs");
+        $userName = filter_input(INPUT_POST, "userName", FILTER_UNSAFE_RAW);
+        $password = filter_input(INPUT_POST, "password", FILTER_UNSAFE_RAW);
+        $email = filter_input(INPUT_POST, "email", FILTER_UNSAFE_RAW);
+        $dateOfBirth = filter_input(INPUT_POST, "dateOfBirth", FILTER_UNSAFE_RAW);
+        if ($userDao->checkEmail($email) == false) {
+            if (isset($_POST['worker'])) {
+                $dateOfBirth = new DateTime($dateOfBirth);
+                $id = $userDao->register($userName, $dateOfBirth, $email, $password, 2, "", "u");
+                // echo "helloo" . $id;
+            } else {
+                $dateOfBirth = new DateTime($dateOfBirth);
+                $id = $userDao->register($userName, $dateOfBirth, $email, $password, 1, "", "u");
+                //echo "hello" .$id;
+            }
+        } else {
+            echo "in use";
+        }
+        break;
+}
