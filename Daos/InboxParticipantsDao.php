19d4109d276c8726af89172e38bc1dc513040ce8
Carlson29 <kometacarlson@gmail.com>
2025-02-18 19:28:18 +0000

    adding function to update lastsent and get Other Users Ibp

--- Daos/InboxParticipantsDao.php
+++ Daos/InboxParticipantsDao.php
@@ -3,11 +3,12 @@
 namespace Daos;
 //use 'business\InboxParticipant.php';
 use Daos\Dao;
+use DateTime;
 use InboxParticipant;
 
-require 'Dao.php';
+//require 'Dao.php';
 require '..\business\InboxParticipant.php';
-require '..\business\User.php';
+//require '..\business\User.php';
 
 
 class InboxParticipantsDao extends Dao
@@ -39,10 +40,59 @@ class InboxParticipantsDao extends Dao
         return false;
     }
 
+    public function updateLastSent(int $inboxId,DateTime $lastSent ):bool
+    {
+        $query = "Update inboxparticipants set lastSent=:lastSent where inboxId=:inboxId ";
+        $statement = $this->getConn()->prepare($query);
+        $lastSent =$lastSent->format('Y-m-d H:i:s');
+        $statement->bindValue(':lastSent', $lastSent);
+        $statement->bindValue(':inboxId', $inboxId);
+        try {
+            $statement->execute();
+            $statement->closeCursor();
+            if($statement->rowCount()==1){
+                return true;
+            }
+            // $userId = $this->getConn()->lastInsertId();
+        } catch (PDOException $ex) {
+            echo "An error occurred in updateLastSent" . $ex->getMessage();
+            return false;
+            exit();
+        }
+
+        //$statement->closeCursor();
+        return false;
+    }
+
     public function getInboxParticipants(int $userId):array {
        // $user = new User();
         $query = "Select * from inboxparticipants where userId=:userId and deletedState=false order by lastSent DESC ";
         $statement = $this->getConn()->prepare($query);
+        $statement->bindValue(':userId', $userId);
+        try {
+            $statement->execute();
+        } catch (PDOException $ex) {
+            echo "An error occurred  on getInboxParticipants" . $ex->getMessage();
+            exit();
+        }
+        $ibps = $statement->fetchAll();
+        $ibpsArray =[];
+        //array_splice($ibpsArray, 0);
+        foreach ($ibps as $ibp){
+            $Ibp= new InboxParticipant();
+            $Ibp->inboxParticipant($ibp[0],$ibp[1],$ibp[2],$ibp[3],$ibp[4]);
+            //$ibpsArray =$Ibp;
+            array_push($ibpsArray, $Ibp);
+        }
+        return $ibpsArray;
+    }
+
+
+    public function getOtherIbps(int $inboxId, int $userId):array {
+        // $user = new User();
+        $query = "Select * from inboxparticipants where inboxId=:inboxId and userId=!userId deletedState=false order by lastSent DESC ";
+        $statement = $this->getConn()->prepare($query);
+        $statement->bindValue(':inboxId', $inboxId);
         $statement->bindValue(':userId', $userId);
         try {
             $statement->execute();
@@ -68,7 +118,7 @@ class InboxParticipantsDao extends Dao
             $query = "Update inboxparticipants set unseenMessages=0 where userId=:userId and inboxId=:inboxId";
         }
         else{
-            $query = "Update inboxparticipants set unseenMessages=unseenMessages+1 where userId=:userId and inboxId=:inboxId";
+            $query = "Update inboxparticipants set unseenMessages=unseenMessages+1 where userId!=:userId and inboxId=:inboxId and isopen=false ";
         }
         $statement = $this->getConn()->prepare($query);
         $statement->bindValue(':userId', $userId);
@@ -76,7 +126,7 @@ class InboxParticipantsDao extends Dao
         try {
             $statement->execute();
             $statement->closeCursor();
-            if($statement->rowCount()==1){
+            if($statement->rowCount()>=1){
                 return true;
             }
             // $userId = $this->getConn()->lastInsertId();
@@ -89,6 +139,8 @@ class InboxParticipantsDao extends Dao
         //$statement->closeCursor();
         return false;
     }
+
+
     public function updateIsOpen(int $userId,int $inboxId, bool $isOpen):bool
     {
         $query = "Update inboxparticipants set isOpen=:isOpen where userId=:userId and inboxId=:inboxId";
@@ -121,4 +173,4 @@ $ibpDao = new InboxParticipantsDao("fastjobs");
 
 //$state=$ibpDao->updateIsOpen(1,1,false);
 $state=$ibpDao->updateUnseenMessages(1,1,0);
-var_dump($state);
+//var_dump($state);
