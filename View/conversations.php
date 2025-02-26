9afd76bf30344b7c7fba3bead1d53e715e3d97c1
Carlson29 <kometacarlson@gmail.com>
2025-02-26 19:25:01 +0000

    adding headers and styling scrollbar

--- View/conversations.php
+++ View/conversations.php
@@ -7,64 +7,133 @@ and open the template in the editor.
 <html>
 <head>
     <style>
-        body{
-            display:flex;
-            flex-direction:row;
+        body {
+        overflow-y: hidden;
         }
-        #ibp{
-            background-color:whitesmoke;
-            display:flex;
-            flex-direction:column;
-            width:33vw;
+
+        #conversationContents {
+            display: flex;
+            flex-direction: row;
+        }
+
+        #ibpSectionHeader {
+            position: relative;
+            width: 33vw;
+            height: 5vh;
+            background-color: whitesmoke;
+            margin-bottom: 0.5vh;
+        }
+        #messageHeader{
+
+        }
+
+        #ibp {
+            background-color: whitesmoke;
+            display: flex;
+            flex-direction: column;
+            width: 33vw;
             height: 15vh;
             border-bottom: 1px solid black;
             position: relative;
-           /* overflow: hidden;
-            text-overflow: ellipsis;*/
+            /* overflow: hidden;
+             text-overflow: ellipsis;*/
         }
-        div #lastMessage{
+
+        div #lastMessage {
             /*text-align:center;*/
             align-self: end;
             max-height: 2.5em;
-           position: absolute;
-             bottom: 0;
+            position: absolute;
+            bottom: 0;
             width: 27vw;
             overflow: hidden;
             text-overflow: ellipsis;
         }
-     #ibpName{
-         top: 1vw;
-         align-self: center;
-        height: 10vh;
-         width: 12vw;
-         font-weight: bold;
-         overflow: hidden;
-         text-overflow: ellipsis;
-     }
-     #ibpPic{
-         height: 8vh;
-         width: 5vw;
-         position: absolute;
-         bottom: 2vw;
-         margin-left: 0.5vw;
-         border-radius:5vw;
-     }
-     #ibpTimeSent{
-         position: absolute;
-         top: 0.5vw;
-         right: 1vw;
-     }
-     #ibpUnseenMessages{
-         position: absolute;
-         top: 2.5vw;
-         right: 1vw;
-         height: 3vh;
-         width: 3vw;
-         background-color: black;
-         color: whitesmoke;
-         border-radius: 2vw;
-         text-align: center;
-     }
+
+        #ibpName {
+            top: 1vw;
+            align-self: center;
+            height: 10vh;
+            width: 12vw;
+            font-weight: bold;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        #ibpPic {
+            height: 8vh;
+            width: 5vw;
+            position: absolute;
+            bottom: 2vw;
+            margin-left: 0.5vw;
+            border-radius: 5vw;
+        }
+
+        #ibpTimeSent {
+            position: absolute;
+            top: 0.5vw;
+            right: 1vw;
+        }
+
+        #ibpUnseenMessages {
+            position: absolute;
+            top: 2.5vw;
+            right: 1vw;
+            height: 3vh;
+            width: 3vw;
+            background-color: black;
+            color: whitesmoke;
+            border-radius: 2vw;
+            text-align: center;
+        }
+
+        #conversationBox {
+            /* overflow-y: scroll;*/
+        }
+
+        #conversationFooter {
+            background-color: whitesmoke;
+            height: 7vh;
+            width: 63vw;
+            bottom: 1vw;
+            text-align: center;
+            position: fixed;
+            right: 1vw;
+        }
+
+        #inputSection {
+            right: 6vw;
+            position: absolute;
+            bottom: 1vw;
+        }
+
+        #ibpSection {
+            overflow-y: scroll;
+            position: relative;
+            height: 92vh;
+
+            &::-webkit-scrollbar {
+                /*width: 0.5em;
+                height: 0.5em;*/
+            }
+
+            &::-webkit-scrollbar-track {
+                bottom: 0;
+                position: absolute;
+
+            }
+
+            &::-webkit-scrollbar-thumb {
+                background-color: rgba(255, 255, 255, .1);
+                border-radius: 3px;
+
+                &:hover {
+                    /* background: rgba(255,255,255,.2);*/
+                    background-color: #555;
+                }
+            }
+
+        }
 
     </style>
     <title><?php
@@ -77,21 +146,85 @@ and open the template in the editor.
 
 
 <body>
-<div id="ibpSection">
-   <div id="ibp"> <div > <img id="ibpPic" src="../defaultPic/default.jpg"> </div> <div id="ibpName"><p>Craic</p></div><p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p> <div id="ibpTimeSent"> <p >19:56</p></div> <div id=""><p id="ibpUnseenMessages">1</p></div> </div>
-    <div id="ibp"><div > <img id="ibpPic" src="../defaultPic/default.jpg"> </div> <div id="ibpName"><p>Craic</p></div> <p id="lastMessage">hey</p> <div id="ibpTimeSent"> <p >19:56</p></div> <div id=""><p id="ibpUnseenMessages">1</p></div> </div>
+<div id="headers">
+<div id="ibpSectionHeader">
+
 </div>
-<div id="conversationBox">
-    <div id="conversationBody">
+<div id="messageHeader">
+
+</div>
+</div>
+<div id="conversationContents">
+    <div id="ibpSection">
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">hey</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+        <div id="ibp">
+            <div><img id="ibpPic" src="../defaultPic/default.jpg"></div>
+            <div id="ibpName"><p>Craic</p></div>
+            <p id="lastMessage">heyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyy
+                yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            <div id="ibpTimeSent"><p>19:56</p></div>
+            <div id=""><p id="ibpUnseenMessages">1</p></div>
+        </div>
+    </div>
+    <div id="conversationBox">
+        <div id="conversationBody">
+        </div>
+
+        <div id="conversationFooter">
+            <div id="inputSection">
+                <input type="file" id="messageFile">
+                <input type="text" id="messageEntered">
+                <button onclick="sendMessage()"> send message</button>
+            </div>
+        </div>
 
     </div>
-
-    <div>
-        <input type="file" id="messageFile">
-        <input type="text" id="messageEntered">
-        <button onclick="sendMessage()"> send message</button>
-    </div>
-
 </div>
 </body>
 <script>
