d2a1e8f0fe69a10047bb9cf3248fbd171d58e747
Carlson29 <kometacarlson@gmail.com>
2025-03-11 00:38:12 +0000

    started creating clientHome.php with style

--- View/clientHome.php
+++ View/clientHome.php
@@ -1,9 +1,72 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<?php
+include 'header.php';
+?>
+<style>
+    .sidenav {
+        height: 100%;
+        width: 4vw;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        right: 0;
+        background-color: #111;
+        overflow-x: hidden;
+        transition: 0.5s;
+        padding-top: 60px;
+        margin-left: 97vw;
+    }
+
+    .sidenav a {
+        padding: 8px 15px 8px 10px;
+        text-decoration: none;
+        font-size: 16px;
+        color: #818181;
+        display: block;
+        transition: 0.3s;
+    }
+
+    .sidenav a:hover {
+        color: #f1f1f1;
+    }
+
+    .sidenav .closebtn {
+        position: absolute;
+        top: 0;
+        font-size: 36px;
+        margin-right:16vw;
+        margin-left: 0px;
+    }
+
+</style>
+<body>
+<div id="sidebar">
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+        <a href="../Controller/index.php?action=show_conversations" class="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-text-fill" viewBox="0 0 16 16">
+                <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM3.5 3h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1z"/>
+            </svg> chats </a>
+        <a href=""class="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+            </svg>profile</a>
+        <a href="" class="button">offers</a>
+    </div>
+
+</div>
 <button><a href="../Controller/index.php?action=show_conversations" >hello</a></button>
-</html>
+</body>
+    <script>
+
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "4vw";
+            document.getElementById("main").style.marginRight = "4vw";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+        }
+        </script>
+<?php
+include 'footer.php';
+?>
