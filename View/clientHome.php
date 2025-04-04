84bb282720929167333c3fab10171d7b70a6b235
Carlson29 <kometacarlson@gmail.com>
2025-04-04 21:16:14 +0100

    removing sidebar from home page

--- View/clientHome.php
+++ View/clientHome.php
@@ -2,41 +2,7 @@
 include 'header.php';
 ?>
     <style>
-        .sidenav {
-            height: 100%;
-            width: 4vw;
-            position: fixed;
-            z-index: 1;
-            top: 0;
-            left: 0;
-            background-color: #111;
-            overflow-x: hidden;
-            transition: 0.5s;
-            /*  padding-top: 5vh;
-              margin-left: 97vw;*/
-        }
 
-        .sidenav a {
-            /*padding: 8px 15px 8px 10px;*/
-            padding: 1vw 1vw 1vw 1vw;
-            text-decoration: none;
-            font-size: 1.2vw;
-            color: #818181;
-            transition: 0.3s;
-            flex-direction: column;
-        }
-
-        .sidenav a:hover {
-            color: #f1f1f1;
-        }
-
-        .sidenav .closebtn {
-            /* position: absolute;
-             margin-right: 16vw;
-             margin-left: 0px;
-             top: 2vh;*/
-            font-size: 3vw;
-        }
 
         #workers {
             display: flex;
@@ -219,24 +185,7 @@ overflow: visible;
 
 
     <body onload="getLocation()" id="main" >
-    <div id="sidebar">
-        <div id="mySidenav" class="sidenav">
-            <a href="" class="closebtn" onclick="closeNav()">&times;</a>
 
-            <a href="../Controller/index.php?action=show_conversations" class="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     class="bi bi-chat-right-text-fill" viewBox="0 0 16 16">
-                    <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM3.5 3h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1z"/>
-                </svg>
-                chats </a>
-            <a href="../Controller/index.php?action=show_Profile" class="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     class="bi bi-person-fill" viewBox="0 0 16 16">
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                </svg>
-                profile</a>
-            <a href="" class="button">offers</a>
-        </div>
 
     </div>
 <div id="search-suggest">
@@ -270,7 +219,7 @@ overflow: visible;
     </body>
     <script>
       // setInterval(getUsersByLocation, 2000);
-        function openNav() {
+       /* function openNav() {
             document.getElementById("mySidenav").style.width = "4vw";
             document.getElementById("main").style.marginLeft = "4vw";
         }
@@ -278,7 +227,7 @@ overflow: visible;
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginRight = "0";
-        }
+        } */
 
         function getLocation() {
             closeNav();
