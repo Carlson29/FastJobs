895518f7847ae275d6b4d94ea4b3ca28c0a9669d
Carlson29 <kometacarlson@gmail.com>
2025-04-18 18:52:03 +0100

    adding style to footer and header

--- View/workerFooter.php
+++ View/workerFooter.php
@@ -15,5 +15,6 @@ and open the template in the editor.
 <script>
 
 </script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
 </body>
 </html>
--- View/workerHeader.php
+++ View/workerHeader.php
@@ -7,7 +7,7 @@
            height: 8vh;
            /*#3b3c36*/
            background-color: #4C676B;
-           width: 100%;
+           width: 99vw;
            overflow: hidden;
        }
        #foot {
@@ -16,7 +16,7 @@
            position: fixed;
            bottom: 0;
            color: #3b3c36;
-           width: 100%;
+           width: 99vw;
        }
 
    </style>
@@ -24,6 +24,14 @@
 <title>
 
 </title>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
     <div id="header">
     </div>
 
